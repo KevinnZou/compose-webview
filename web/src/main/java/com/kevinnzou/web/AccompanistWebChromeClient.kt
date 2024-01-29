4941d48a9ddda5ab1e31d773e07d193fe7ecf2a1
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:28:49 +0800

    feat:AccompanistWebChromeClient

---
+++ web/src/main/java/com/kevinnzou/web/AccompanistWebChromeClient.kt
@@ -0,0 +1,39 @@
+package com.kevinnzou.web
+
+import WebViewState
+import android.graphics.Bitmap
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+
+/**
+ * Created By Kevin Zou On 2024/1/29
+ */
+
+/**
+ * AccompanistWebChromeClient
+ *
+ * A parent class implementation of WebChromeClient that can be subclassed to add custom behaviour.
+ *
+ * As Accompanist Web needs to set its own web client to function, it provides this intermediary
+ * class that can be overriden if further custom behaviour is required.
+ */
+public open class AccompanistWebChromeClient : WebChromeClient() {
+    public open lateinit var state: WebViewState
+        internal set
+
+    override fun onReceivedTitle(view: WebView, title: String?) {
+        super.onReceivedTitle(view, title)
+        state.pageTitle = title
+    }
+
+    override fun onReceivedIcon(view: WebView, icon: Bitmap?) {
+        super.onReceivedIcon(view, icon)
+        state.pageIcon = icon
+    }
+
+    override fun onProgressChanged(view: WebView, newProgress: Int) {
+        super.onProgressChanged(view, newProgress)
+        if (state.loadingState is LoadingState.Finished) return
+        state.loadingState = LoadingState.Loading(newProgress / 100.0f)
+    }
+}
