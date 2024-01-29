1ef3bea0ff220064c6d75f53688f60658eef8063
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:28:02 +0800

    feat:LoadingState

---
+++ web/src/main/java/com/kevinnzou/web/LoadingState.kt
@@ -0,0 +1,22 @@
+
+/**
+ * Sealed class for constraining possible loading states.
+ * See [Loading] and [Finished].
+ */
+public sealed class LoadingState {
+    /**
+     * Describes a WebView that has not yet loaded for the first time.
+     */
+    public data object Initializing : LoadingState()
+
+    /**
+     * Describes a webview between `onPageStarted` and `onPageFinished` events, contains a
+     * [progress] property which is updated by the webview.
+     */
+    public data class Loading(val progress: Float) : LoadingState()
+
+    /**
+     * Describes a webview that has finished loading content.
+     */
+    public data object Finished : LoadingState()
+}
