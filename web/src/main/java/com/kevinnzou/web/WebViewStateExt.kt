8f9f6a2a8e96efb79d04393b7df1f79b67169e91
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:28:28 +0800

    feat:WebViewStateExt

---
+++ web/src/main/java/com/kevinnzou/web/WebViewStateExt.kt
@@ -0,0 +1,123 @@
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.kevinnzou.web.WebView
+
+/**
+ * Creates a WebView state that is remembered across Compositions.
+ *
+ * @param url The url to load in the WebView
+ * @param additionalHttpHeaders Optional, additional HTTP headers that are passed to [WebView.loadUrl].
+ *                              Note that these headers are used for all subsequent requests of the WebView.
+ */
+@Composable
+public fun rememberWebViewState(
+    url: String,
+    additionalHttpHeaders: Map<String, String> = emptyMap()
+): WebViewState =
+// Rather than using .apply {} here we will recreate the state, this prevents
+    // a recomposition loop when the webview updates the url itself.
+    remember {
+        WebViewState(
+            WebContent.Url(
+                url = url,
+                additionalHttpHeaders = additionalHttpHeaders
+            )
+        )
+    }.apply {
+        this.content = WebContent.Url(
+            url = url,
+            additionalHttpHeaders = additionalHttpHeaders
+        )
+    }
+
+/**
+ * Creates a WebView state that is remembered across Compositions.
+ *
+ * @param data The uri to load in the WebView
+ */
+@Composable
+public fun rememberWebViewStateWithHTMLData(
+    data: String,
+    baseUrl: String? = null,
+    encoding: String = "utf-8",
+    mimeType: String? = null,
+    historyUrl: String? = null
+): WebViewState =
+    remember {
+        WebViewState(WebContent.Data(data, baseUrl, encoding, mimeType, historyUrl))
+    }.apply {
+        this.content = WebContent.Data(
+            data, baseUrl, encoding, mimeType, historyUrl
+        )
+    }
+
+/**
+ * Creates a WebView state that is remembered across Compositions.
+ *
+ * @param url The url to load in the WebView
+ * @param postData The data to be posted to the WebView with the url
+ */
+@Composable
+public fun rememberWebViewState(
+    url: String,
+    postData: ByteArray
+): WebViewState =
+// Rather than using .apply {} here we will recreate the state, this prevents
+    // a recomposition loop when the webview updates the url itself.
+    remember {
+        WebViewState(
+            WebContent.Post(
+                url = url,
+                postData = postData
+            )
+        )
+    }.apply {
+        this.content = WebContent.Post(
+            url = url,
+            postData = postData
+        )
+    }
+
+/**
+ * Creates a WebView state that is remembered across Compositions and saved
+ * across activity recreation.
+ * When using saved state, you cannot change the URL via recomposition. The only way to load
+ * a URL is via a WebViewNavigator.
+ *
+ * @param data The uri to load in the WebView
+ * @sample com.google.accompanist.sample.webview.WebViewSaveStateSample
+ */
+@Composable
+public fun rememberSaveableWebViewState(): WebViewState =
+    rememberSaveable(saver = WebStateSaver) {
+        WebViewState(WebContent.NavigatorOnly)
+    }
+
+public val WebStateSaver: Saver<WebViewState, Any> = run {
+    val pageTitleKey = "pagetitle"
+    val lastLoadedUrlKey = "lastloaded"
+    val stateBundle = "bundle"
+
+    mapSaver(
+        save = {
+            val viewState = Bundle().apply { it.webView?.saveState(this) }
+            mapOf(
+                pageTitleKey to it.pageTitle,
+                lastLoadedUrlKey to it.lastLoadedUrl,
+                stateBundle to viewState
+            )
+        },
+        restore = {
+            WebViewState(WebContent.NavigatorOnly).apply {
+                this.pageTitle = it[pageTitleKey] as String?
+                this.lastLoadedUrl = it[lastLoadedUrlKey] as String?
+                this.viewState = it[stateBundle] as Bundle?
+            }
+        }
+    )
+}
