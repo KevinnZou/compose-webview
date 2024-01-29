9f502d9be70560fefe13cc76f9a7f3c5e7c0e4a1
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:27:34 +0800

    feat:WebViewError

---
+++ web/src/main/java/com/kevinnzou/web/WebViewError.kt
@@ -0,0 +1,18 @@
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import androidx.compose.runtime.Immutable
+
+/**
+ * A wrapper class to hold errors from the WebView.
+ */
+@Immutable
+public data class WebViewError(
+    /**
+     * The request the error came from.
+     */
+    val request: WebResourceRequest?,
+    /**
+     * The error that was reported.
+     */
+    val error: WebResourceError
+)
