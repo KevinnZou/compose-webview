0716fc3729fc20ac40b71f1a5d98ed1091a04838
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:28:12 +0800

    feat:WebViewState

---
+++ web/src/main/java/com/kevinnzou/web/WebViewState.kt
@@ -0,0 +1,68 @@
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
+/**
+ * A state holder to hold the state for the WebView. In most cases this will be remembered
+ * using the rememberWebViewState(uri) function.
+ */
+@Stable
+public class WebViewState(webContent: WebContent) {
+    public var lastLoadedUrl: String? by mutableStateOf(null)
+        internal set
+
+    /**
+     *  The content being loaded by the WebView
+     */
+    public var content: WebContent by mutableStateOf(webContent)
+
+    /**
+     * Whether the WebView is currently [LoadingState.Loading] data in its main frame (along with
+     * progress) or the data loading has [LoadingState.Finished]. See [LoadingState]
+     */
+    public var loadingState: LoadingState by mutableStateOf(LoadingState.Initializing)
+        internal set
+
+    /**
+     * Whether the webview is currently loading data in its main frame
+     */
+    public val isLoading: Boolean
+        get() = loadingState !is LoadingState.Finished
+
+    /**
+     * The title received from the loaded content of the current page
+     */
+    public var pageTitle: String? by mutableStateOf(null)
+        internal set
+
+    /**
+     * the favicon received from the loaded content of the current page
+     */
+    public var pageIcon: Bitmap? by mutableStateOf(null)
+        internal set
+
+    /**
+     * A list for errors captured in the last load. Reset when a new page is loaded.
+     * Errors could be from any resource (iframe, image, etc.), not just for the main page.
+     * For more fine grained control use the OnError callback of the WebView.
+     */
+    public val errorsForCurrentRequest: SnapshotStateList<WebViewError> = mutableStateListOf()
+
+    /**
+     * The saved view state from when the view was destroyed last. To restore state,
+     * use the navigator and only call loadUrl if the bundle is null.
+     * See WebViewSaveStateSample.
+     */
+    public var viewState: Bundle? = null
+        internal set
+
+    // We need access to this in the state saver. An internal DisposableEffect or AndroidView
+    // onDestroy is called after the state saver and so can't be used.
+    internal var webView by mutableStateOf<WebView?>(null)
+}
