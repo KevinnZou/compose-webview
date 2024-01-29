0d26044ac74e6ae22314061a9d67cd8261d5e276
邹铭杰 <zoumingjie17@163.com>
2024-01-29 16:28:41 +0800

    feat:AccompanistWebViewClient

---
+++ web/src/main/java/com/kevinnzou/web/AccompanistWebViewClient.kt
@@ -0,0 +1,63 @@
+package com.kevinnzou.web
+
+import WebViewError
+import WebViewNavigator
+import WebViewState
+import android.graphics.Bitmap
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+/**
+ * Created By Kevin Zou On 2024/1/29
+ */
+
+/**
+ * AccompanistWebViewClient
+ *
+ * A parent class implementation of WebViewClient that can be subclassed to add custom behaviour.
+ *
+ * As Accompanist Web needs to set its own web client to function, it provides this intermediary
+ * class that can be overriden if further custom behaviour is required.
+ */
+open class AccompanistWebViewClient : WebViewClient() {
+    public open lateinit var state: WebViewState
+        internal set
+    public open lateinit var navigator: WebViewNavigator
+        internal set
+
+    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+        state.loadingState = LoadingState.Loading(0.0f)
+        state.errorsForCurrentRequest.clear()
+        state.pageTitle = null
+        state.pageIcon = null
+
+        state.lastLoadedUrl = url
+    }
+
+    override fun onPageFinished(view: WebView, url: String?) {
+        super.onPageFinished(view, url)
+        state.loadingState = LoadingState.Finished
+    }
+
+    override fun doUpdateVisitedHistory(view: WebView, url: String?, isReload: Boolean) {
+        super.doUpdateVisitedHistory(view, url, isReload)
+
+        navigator.canGoBack = view.canGoBack()
+        navigator.canGoForward = view.canGoForward()
+    }
+
+    override fun onReceivedError(
+        view: WebView,
+        request: WebResourceRequest?,
+        error: WebResourceError?
+    ) {
+        super.onReceivedError(view, request, error)
+
+        if (error != null) {
+            state.errorsForCurrentRequest.add(WebViewError(request, error))
+        }
+    }
+}
